20e765396c96e139cdbe834f1b0de36877e65f91
amarti65 <amarti65@kent.edu>
2014-10-12 18:31:56 -0400

    Contains Lab_3_NewActivity

---
+++ Lab_3_NewActivity/app/src/main/java/com/example/anthony/lab_3_newactivity/TheNewActivity.java
@@ -0,0 +1,43 @@
+package com.example.anthony.lab_3_newactivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class TheNewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_the_new);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.the_new, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void launchNewActivity(View view) {
+        final Intent intent = new Intent("com.639.tryintent");
+        startActivity(intent);
+    }
+}
